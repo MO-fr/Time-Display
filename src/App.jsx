e7df44d8e17c9049e52623f39b58f8fb43e44a0f
Mohamed <msoua0040@launchpadphilly.org>
2025-02-12 19:53:58 -0500

    Changed the name of the timer shown on the site itself

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Timerpage from './components/Timerpage.jsx'; // Import your Timerpage com
 function App() {
   return (
     <div className="App">
-      <h1 className='title'>The Focus Stopwatch!</h1>
+      <h1 className='title'>The Focus Tracker!</h1>
       <Timerpage /> 
     </div>
   );
